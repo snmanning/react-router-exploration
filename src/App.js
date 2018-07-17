1cafa1b8c8f65ae8c2be4bee9bc05372bd13f4cb
Sarah Manning <sarahnmanning@gmail.com>
2018-07-16 21:05:49 -0400

    worked around with links and routers rearranging them within the docs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import {
+  Link,
+  Route
+} from 'react-router-dom';
 import './App.css';
+
+const Navigation = (props) => (
+  <ul>
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/about'>About</Link></li>
+    <li><Link to='/contact'>Contact</Link></li>
+  </ul>
+);
+
+// route : '/'
+const Home = (props) => (
+    <section> This is your home. Do you not like your home? </section>
+);
+
+// route: '/about'
+const About = (props) => (
+    <section> Something about me </section>
+);
+
+// route: '/contact'
+const Contact = (props) => (
+    <section> Call me, maybe. </section>
+);
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        It works!!!
-      </div>
+            <div>
+              <Navigation/>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/contact' component={Contact} />
+            </div>
     );
   }
 }
